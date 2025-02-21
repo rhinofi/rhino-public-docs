e74fe8a2bd225cf6c5093f5630e23e0bf5d237c4
matko95 <mtrifunovski@gmail.com>
2025-02-21 17:54:38 +0100

    add ca page

--- scripts/getContractAddresses.ts
+++ scripts/getContractAddresses.ts
@@ -1,12 +1,12 @@
-const getSupportedChains = async () => {
+const getContractAddresses = async () => {
   const result = await fetch('https://api.rhino.fi/bridge/configs')
   const data = await result.json()
-  const header = '| Chain | Tokens |'
+  const header = '| Chain | Contract address |'
 
   // console.log(Object.keys(data).map(key => `- ${data[key].name} (${Object.keys(data[key].tokens).join(', ')})`).join('\n'))
   console.log(header)
   console.log('|---|---|')
-  console.log(Object.keys(data).map(key => `| ${data[key].name} | ${Object.keys(data[key].tokens).join(', ')} |`).join('\n'))
+  console.log(Object.keys(data).map(key => `| ${data[key].name} | ${data[key].contractAddress} |`).join('\n'))
 }
 
-getSupportedChains()
+getContractAddresses()
