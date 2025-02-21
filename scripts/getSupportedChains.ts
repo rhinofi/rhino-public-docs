00b7a703f9c922b3660b6ee18cacc7e5f11edd66
matko95 <mtrifunovski@gmail.com>
2025-02-21 17:40:19 +0100

    add ca to docs

--- scripts/getSupportedChains.ts
+++ scripts/getSupportedChains.ts
@@ -1,12 +1,12 @@
 const getSupportedChains = async () => {
   const result = await fetch('https://api.rhino.fi/bridge/configs')
   const data = await result.json()
-  const header = '| Chain | Tokens |'
+  const header = '| Chain | Tokens | Bridge contract address'
 
   // console.log(Object.keys(data).map(key => `- ${data[key].name} (${Object.keys(data[key].tokens).join(', ')})`).join('\n'))
   console.log(header)
-  console.log('|---|---|')
-  console.log(Object.keys(data).map(key => `| ${data[key].name} | ${Object.keys(data[key].tokens).join(', ')} |`).join('\n'))
+  console.log('|---|---|---|')
+  console.log(Object.keys(data).map(key => `| ${data[key].name} | ${Object.keys(data[key].tokens).join(', ')} | ${data[key].contractAddress}`).join('\n'))
 }
 
 getSupportedChains()
