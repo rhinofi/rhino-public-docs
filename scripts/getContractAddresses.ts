d2894ae32837002056351e200c147d9d8b748650
Jannis Isensee <jannis.isensee@gmail.com>
2025-08-29 09:38:35 +0200

    update supported chains

--- scripts/getContractAddresses.ts
+++ scripts/getContractAddresses.ts
@@ -2,11 +2,11 @@ const getContractAddresses = async () => {
   const result = await fetch('https://api.rhino.fi/bridge/configs')
   const data = await result.json()
   const header = '| Chain | Contract address |'
+  const enabledChains = Object.values(data).filter(chain => chain.status === 'enabled')
 
-  // console.log(Object.keys(data).map(key => `- ${data[key].name} (${Object.keys(data[key].tokens).join(', ')})`).join('\n'))
   console.log(header)
   console.log('|---|---|')
-  console.log(Object.keys(data).map(key => `| ${data[key].name} | ${data[key].contractAddress} |`).join('\n'))
+  console.log(enabledChains.map(chain => `| ${chain.name} | ${chain.contractAddress} |`).join('\n'))
 }
 
 getContractAddresses()
